0865149779602772a1c1399f0b869ab3a4e522f2
Jessica Pena <jcpena3775@gmail.com>
2020-11-04 18:14:05 -0600

    update

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -9,21 +9,63 @@ function plotlystart() {
     })
  }
 
-plotlystart()
+plotlystart();
 // create demographic data
 function metadataLoading(userID) {
     
-    d3.json("samples.json").then((metaData) => {
-        var metadataBody = d3.select("#sample-metadata"); 
-        var infofiltered = metaData.metadata.filter(person => person.id === userID)
-        var firstID = infofiltered[0] 
-        console.log
+    d3.json("samples.json").then((data) => {
         
-        Object.entries(firstID).forEach(([key,value])=> {
-            metadataBody.append("p").text(`${key} ${value}`) 
-        })
+        var infofiltered = data.metadata.filter(person => person.id == userID)
+        var firstID = infofiltered[0] 
+        console.log(firstID);
+        var metadataBody = d3.select("#sample-metadata"); 
+        metadataBody.html("")
+        Object.entries(firstID).forEach(function([key,value]) {
+            var metaBody =  metadataBody.append("p")
+            metaBody.text(`${key} ${value}`) 
+        });
+// // Creating charts
+// d3.json("samples.json").then((sampleData) => {
+//     var samples = sampleData.samples
+//     console.log(samples)
+
 })
-}
+};
+
 function optionChanged(userID){
     metadataLoading(userID)
 };
+
+
+
+// Creating charts
+// var drawChart = function(xData, yData, hoverText, metaData) {
+//     var mdPanel = d3.select("#sample-metadata");
+//     mdPanel.html("");
+
+    // Object.entries(metaData).forEach(([key, value]) => {
+    //     mdPanel.append("p").text(`${key}: ${value}`);
+    // });
+  
+    // var trace = {
+    //     x: xData,
+    //     y: yData,
+    //     text: hoverText,
+    //     type: 'bar',
+    //     orientation: 'h'
+    // };
+  
+    // var data = [trace];
+  
+    // Plotly.newPlot('bar', data);
+  
+    // var trace2 = {
+    //     x: xData,
+    //     y: yData,
+    //     text: hoverText,
+    //     mode: 'markers',
+    //     marker: {
+    //         size: yData,
+    //         color: xData
+    //     }
+    // }};
